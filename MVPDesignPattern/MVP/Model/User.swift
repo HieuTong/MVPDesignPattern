1f1c7879bcb6a934526423cf13aa1fcfe4baa138
HieuTong <hieu.le@coinhako.com>
2022-07-30 16:10:10 +0700

    Learn project

---
+++ MVPDesignPattern/MVP/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MVPDesignPattern
+//
+//  Created by Le Tong Minh Hieu on 30/07/2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let username: String
+}
